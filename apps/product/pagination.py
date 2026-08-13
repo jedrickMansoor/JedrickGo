bc40aa35e2bb6ca6160ecba4cd832b033a20574e
Jedrick <empman26@gmail.com>
2026-08-13 13:38:15 +0500

    Qauntity corrected and defaul page set to 1

--- apps/product/pagination.py
+++ apps/product/pagination.py
@@ -6,7 +6,6 @@ class ProductPagination(PageNumberPagination):
     page_query_param = "page"
 
     def paginate_queryset(self, queryset, request, view=None):
-        page = request.query_params.get("page")
         quantity = request.query_params.get("quantity")
 
         if quantity:
@@ -19,11 +18,9 @@ class ProductPagination(PageNumberPagination):
             except (ValueError, TypeError):
                 pass
 
-        if page is None:
-            if quantity:
-                return list(queryset)
-
-            return None
+        if "page" not in request.query_params:
+            request._request.GET = request._request.GET.copy()
+            request._request.GET["page"] = "1"
 
         return super().paginate_queryset(
             queryset,
